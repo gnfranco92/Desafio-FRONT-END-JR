d94222576367d2746948da47d819299ffef24bca
Guilherme Neto Franco <guilhermenetofranco@ymail.com>
2021-05-04 10:53:58 -0300

    JavaScript

--- validar.js
+++
@@ -1,8 +0,0 @@
-var salvarNome = function () {
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-
-    console.log(nome + email);
-}
-
-document.onclick = salvarNome;
